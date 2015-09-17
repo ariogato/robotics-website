1eee620e24a8be2fbe4bce0c06290b1262a11c29
ariogato <ariodddddd@yahoo.de>
2015-09-18 00:19:23 +0200

    1. Created "index.php" 2. Created "home.css" inside of "css/". (File's still empty) 3. added most of the important tags inside of "<head>" in "index.php"

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!--title for the page-->
+		<title>Homepage der LFG-robotics - Startseite</title>
+		
+		<!--charset-->
+		<meta charset="UTF-8">
+		
+		<!--linking to the stylesheet-->
+		<link rel="stylesheet" href="css/home.css">
+		
+		<!--General Information about the site-->
+		<meta name="author" content="Ario Dastmaltschi & Ben Stickelbrucks">
+		<meta name="describtion" content="Vorstellung des student-robotics Teams vom Lion-Feuchtwanger-Gymnasium in München.">
+		<meta name="keywords" content=""> <!--Don't know what toput in here-->
+	</head>
+	<body>
+		
+	</body>	
+</html>
